a74f53a56d123721e138ae43c69a93dbedd79d4a
VNCT <tai_cao@fastboy.net>
2023-02-18 02:16:11 +0700

    lai_suat

---
+++ Tinh_lai_suat/tinh_lai.js
@@ -0,0 +1,9 @@
+sotienvay = prompt("Số tiền vay");
+laisuat = prompt("lãi suất");
+sonam = prompt("Số năm");
+laisuatphantram = laisuat/100;
+laidon = sotienvay * laisuatphantram;
+
+laisuatnganhang =  +sotienvay +  +sonam * +laidon ;
+alert(laisuatnganhang);
+alert(laidon);
