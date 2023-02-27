87e51b138e53dc1b49f8d6dd7c766849fd09651e
VNCT <tai_cao@fastboy.net>
2023-02-27 19:54:09 +0700

    Bài 1

---
+++ 27-2-2023/practice_1.js
@@ -0,0 +1,7 @@
+// Bài 1: Hãy viết một chương trình để đảo ngược các phần tử trong list
+let aList = [100, 200, 300, 400, 500];
+
+// bList = [500, 400, 300, 200, 100]
+let bList = aList;
+bList.reverse();
+document.write(`Mảng blist là ${bList}`);
