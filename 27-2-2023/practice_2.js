14c5a962d8ecbfd91d919f15b9d165877eaca38e
VNCT <tai_cao@fastboy.net>
2023-02-27 20:23:34 +0700

    Bài 2

---
+++ 27-2-2023/practice_2.js
@@ -0,0 +1,10 @@
+//  Bài 2: Hãy tạo ra một mảng  mới bằng cách 
+//cộng các phần tử tương ứng của mảng  cũ theo index tương ứng
+let array1 =["M", "na", "i", "Ke"];
+let array2 =["y", "me", "s", "lly"];
+//['my', 'name', 'is', 'kelly'] 
+let array3 = [];
+for(let  i in array1){
+    array3 = array1[i] + array2[i]+'-';
+    document.write(array3);
+}
