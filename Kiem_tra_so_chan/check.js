80ca8dcc0dd25e6457af91d8f8ee2a71e7353b93
VNCT <tai_cao@fastboy.net>
2023-03-07 00:53:43 +0700

    Kiem tra so chan le

--- Kiem_tra_so_chan/check.js
+++ Kiem_tra_so_chan/check.js
@@ -1,9 +1,9 @@
-let ip = parseInt(document.getElementById("ip"));
-let btaction= document.getElementById("btaction");
-btaction.addEventListener("click", () => {
-    if ( ip % 2 ==0) {
-        alert("chẵn");
-    } else{
-        alert("lẻ");
-    }
-}); 
+let bt_action= document.getElementById("btaction");
+bt_action.addEventListener("click", () => {
+    let ip = parseInt(document.getElementById("id").value);
+   if(ip % 2==0){
+    alert("Chẵn");
+   } else{
+    alert("lẻ");
+   }
+});
